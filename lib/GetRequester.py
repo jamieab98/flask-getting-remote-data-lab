c3d5b8c931ee6bd77c6d7a4d341da7c46f0fbaa1
Jamie Bryson <jamieab98@gmail.com>
2026-02-05 16:40:38 -0500

    d

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -6,14 +6,14 @@ class GetRequester:
     def __init__(self, url):
         self.url = url
 
-    def load_json(self):
+    def get_response_body(self):
         response = requests.get(self.url)
         return response.content
 
-    def get_response_body(self):
+    def load_json(self):
         response = requests.get(self.url)
         data = json.dumps(response.json(), indent=1)
-        return(data)
+        return data
 
 g = GetRequester("https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json")
-print(g.get_response_body())
+print(g.load_json())
